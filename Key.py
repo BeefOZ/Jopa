e6457768013959f684edfcfe98315a0532d6dd70
BeefOZ <alex_017@bk.ru>
2018-07-13 14:55:37 +0300

    0.11

--- Key.py
+++ Key.py
@@ -40,7 +40,7 @@ def on_press(key):
 def on_click(x, y, button, pressed):
     global toggle9,toggle8
     print(button)
-    if str(button) == "Button.button19":
+    if str(button) == "Button.button20":
         if pressed:
             toggle9 = True
         else:
